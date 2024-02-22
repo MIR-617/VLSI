098ef5775098b53205fb45ba75ae878c3dec105f
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-02-22 13:11:15 +0530

    Create or_gate_tb.v

---
+++ Verilog/Day 1/or_gate_tb.v
@@ -0,0 +1,21 @@
+// Code for or_gate testbench 
+module or_gate_tb;
+  reg a,b;
+  wire y;
+  or_gate orgate(.a(a),.b(b),.y(y));
+initial begin
+//monitor and dump instructions
+  $monitor("%t a=%0d b=%0d y=%0d",$time,a,b,y);
+  $dumpfile("or_gate.vcd");
+  $dumpvars(0,or_gate_tb);
+end
+initial begin
+//apply input values
+  a=1'b0;b=1'b0;
+  #10a=1'b0;b=1'b1;
+  #10a=1'b1;b=1'b0;
+  #10a=1'b1;b=1'b1;
+  #10
+  $finish;
+end
+endmodule
