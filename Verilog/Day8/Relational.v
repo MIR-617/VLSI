bfe6ac52bd7138def67ff0edb5b5d4ba453976ac
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-03-01 22:39:21 +0530

    Create Relational.v

---
+++ Verilog/Day8/Relational.v
@@ -0,0 +1,12 @@
+module Relational (A, B, Y1, Y2, Y3, Y4);
+  input [2:0] A, B;
+  output reg Y1, Y2, Y3, Y4;
+  
+  always @(A or B) begin
+    Y1= A<B;
+    Y2= A<=B;
+    Y3= A>B;
+    Y4= A>=B;
+  end
+  
+endmodule
