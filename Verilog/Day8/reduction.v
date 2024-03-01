5bb564f6ade9190b0e4a5988c4daefbc81782128
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-03-01 22:34:27 +0530

    Create reduction.v

---
+++ Verilog/Day8/reduction.v
@@ -0,0 +1,16 @@
+module Reduction(
+  input [3:0] A,
+  output reg y1, y2, y3, y4, y5, y6
+);
+
+always@* begin 
+  y1 = &A;
+  y2 = ~&A;
+  y3 = |A;
+  y4 = ~|A;
+  y5 = ^A;
+  y6 = ~^A;
+  
+end
+
+endmodule
