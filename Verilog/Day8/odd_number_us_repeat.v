c82077d9429ca7bf265f928bddd5526dcc05d603
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-03-01 21:55:22 +0530

    Create odd_number_us_repeat.v

---
+++ Verilog/Day8/odd_number_us_repeat.v
@@ -0,0 +1,11 @@
+module odd_number(
+  input  [31:0]num);
+
+always @(*)
+  begin
+    if(num%2==1)
+      $display("The number is a odd number",num);
+    else
+      $display("The number is not a odd number",num);
+   end 
+endmodule
