67f70ef450a51dc45b894ac5d510b444dfbd90f4
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-03-04 15:18:48 +0530

    Create shift_opration.v

---
+++ Verilog/Day9/shift_opration.v
@@ -0,0 +1,16 @@
+module shift_operator(
+  input [7:0] N,
+  input [4:0] shift_amount,
+  input D,
+  output reg [7:0] out);
+
+    always @(*) begin
+      if (D) begin
+            out = N << shift_amount;
+        end
+        else begin
+            out = N >> shift_amount;
+        end
+    end
+
+endmodule
