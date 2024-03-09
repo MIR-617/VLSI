048d3238bb89ef287d449be52f09c222edc5fe9f
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-03-09 11:24:53 +0530

    Create down_counter.v

---
+++ Verilog/Day_10/down_counter.v
@@ -0,0 +1,16 @@
+module downcounter(
+  input clk, reset,
+  output reg [3:0] out
+);
+  
+  always @(posedge clk or posedge reset) begin
+    
+    if(reset)
+      out <= 4'b1111;
+    
+    else 
+      out <= out - 1;
+    
+  end
+  
+endmodule 
