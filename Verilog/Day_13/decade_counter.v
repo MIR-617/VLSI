437afbed5621a949f5333f1036a2b8e9ac450dc3
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-03-12 19:35:48 +0530

    Create decade_counter.v

---
+++ Verilog/Day_13/decade_counter.v
@@ -0,0 +1,14 @@
+module decade_counter
+  #(parameter N=4)
+  (input clk, reset,
+    output reg [N-1:0] out);
+  always @(posedge clk or posedge reset)
+    begin
+      if (reset)
+        out<=4'b0000;
+      else if(out==4'b1001)
+      out<=4'b0000;
+    else
+      out<=out+1;
+  end 
+endmodule
