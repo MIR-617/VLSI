0e24a60888316fa3169ba62cd514e7e88c2596a4
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-04-12 11:18:52 +0530

    Create dynamic_array_ex3.sv

---
+++ Systemverilog/Day_1/dynamic_array_ex3.sv
@@ -0,0 +1,24 @@
+module dynamic_array;
+  int array[];
+  initial begin 
+    array=new[6];
+    array={1,2,3,4,5,6};
+           foreach(array[i])
+           $display("array[%0d]=%0d",i,array[i]);
+           $display("size of the array=%0d",array.size());
+           array=new[8](array);
+           $display("size of array after resizing =%0d",array.size());
+           foreach(array[i])
+             $display("array[%0d]=%0d",i,array[i]);
+    $display("size of array after overloading=%d",array.size());
+    foreach(array[i])
+      $display("array[%0d]=%0d",i,array[i]);
+    array.delete();
+    $display("the size of array after deleting=%0d",array.size());
+    
+           end
+           endmodule
+           
+  
+
+
