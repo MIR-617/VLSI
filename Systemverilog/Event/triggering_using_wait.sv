56dc71773e3df4c32a10eef377b777c1f389f0d1
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-06-26 18:35:56 +0530

    Create triggering_using_wait.sv

---
+++ Systemverilog/Event/triggering_using_wait.sv
@@ -0,0 +1,30 @@
+module event_ex;
+  event e1;
+  
+  task process_A;
+    //#10
+    $display("------------------------------");
+    $display("Before event trigger");
+    ->e1;
+    $display("------------------------------");
+    $display("After event trigger");
+    $display("------------------------------");
+  endtask
+  
+  task process_B;
+    //#10
+    $display("------------------------------");
+    $display("Waiting for event trigger");
+    //@e1;
+    wait(e1.triggered);
+    $display("Event is triggered");
+    $display("------------------------------");
+  endtask
+  
+  initial begin
+    fork
+    process_A;
+    process_B;
+    join
+  end
+endmodule
