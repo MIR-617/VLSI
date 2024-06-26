7ff4aeaffc0868bb61536561f5dd86bf5afb1b9c
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-06-26 12:46:03 +0530

    Create case_without_default.sv

---
+++ Systemverilog/case_statements/case_without_default.sv
@@ -0,0 +1,14 @@
+module tb;
+  bit[1:0]x;
+  initial begin
+    x=2'b01;
+    case(x)
+      00:$display("The value of x=%b",x);
+      01:$display("The value of x=%b",x);
+      10:$display("The value of x=%b",x);
+      11:$display("The value of x=%b",x);
+     
+    endcase
+  end
+endmodule
+    
