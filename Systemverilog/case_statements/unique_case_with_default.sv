7f39b21647805457d2564b6bdd3806fcfcf223e8
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-06-26 15:04:32 +0530

    Create unique_case_with_default.sv

---
+++ Systemverilog/case_statements/unique_case_with_default.sv
@@ -0,0 +1,13 @@
+module tb;
+  bit[1:0] x;
+  initial begin
+    x=2'b01;
+   unique case(x) inside
+      00:$display("The value of x=%b",x);
+//       01:$display("The value of x=%b",x);
+      10:$display("The value of x=%b",x);
+      11:$display("The value of x=%b",x);
+     default:$display("The value not found");    
+    endcase
+  end
+endmodule
