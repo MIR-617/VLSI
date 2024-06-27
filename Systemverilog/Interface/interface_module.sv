2bc93f52878a165b54c566de05ca757b0a477ff2
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-06-27 14:25:14 +0530

    Delete Systemverilog/Interface directory

--- Systemverilog/Interface/interface_module.sv
+++
@@ -1,4 +0,0 @@
-interface and_if;
-  logic a,b;
-  logic y;
-endinterface
--- Systemverilog/Interface/top.sv
+++
@@ -1,7 +0,0 @@
-`include "testbench.sv"
-
- module top;
-   and_if inf();
-   and_g a1(.a(inf.a),.b(inf.b),.y(inf.y));
-   tb a2(inf);
- endmodule
