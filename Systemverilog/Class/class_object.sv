631c3633f3219c82eadac010a82965908d400340
MIR-617 <130532962+MIR-617@users.noreply.github.com>
2024-06-28 12:21:08 +0530

    Create class_object.sv

---
+++ Systemverilog/Class/class_object.sv
@@ -0,0 +1,20 @@
+class home;
+  string switch;
+  task open_electricity();
+    switch ="on";
+    $display("switch=%s",switch);
+  endtask
+endclass
+  module tb;
+//      home h1;
+    initial begin
+      home h1;
+      h1=new();
+      $display("check wheather the object is created");
+      if(h1==null)
+        $display("object is empty");
+      else
+        $display("object is not empty");
+      
+    end
+  endmodule
